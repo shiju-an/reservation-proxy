17ee6992399d82f407d9d87196d9270369439a1b
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-24 16:49:36 -0700

    Adds final edits to proxy - need code cleanup on individual modules

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@ const app = express()
 const port = 3002;
 const path = require('path');
 const cors = require('cors');
-const httpProxy = require('http-proxy');
-
-const proxy = httpProxy.createProxyServer({});
 
 app.use(cors());
 app.use(express.static(__dirname + '/../dist'));
