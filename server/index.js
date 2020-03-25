ea5ec1ebe9129d8d2f7e812149016f1a9054424e
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-24 17:06:53 -0700

    Minimizes code in server

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3002;
 const path = require('path');
-const cors = require('cors');
 
-app.use(cors());
 app.use(express.static(__dirname + '/../dist'));
 
 
