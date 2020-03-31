21744ede39109fd5863a8c90d4353c655dfd376e
jon-zawada <jonathan_zawada@yahoo.com>
2020-03-31 00:34:51 -0700

    adds dockerfile

--- server/index.js
+++ server/index.js
@@ -5,5 +5,8 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/../dist'));
 
+app.get ('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
+})
 
 app.listen(port, () => console.log(`Proxy server listening on port ${port}!`))
